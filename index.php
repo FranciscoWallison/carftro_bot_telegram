08e4963da1728f5e4437f2c500feb1dfc2f70fc2
Wallison <franciscowallison@gmail.com>
2017-10-06 11:02:00 -0300

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+* Mostra o ID o nome e o sobre nome
+*/
+require 'vendor/autoload.php';
+use Telegram\Bot\Api;
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+
+	$dotenv->load();
+	$telegram = new Api();
+	
+	$response = $telegram->getMe();
+	
+	$botId = $response->getId();
+	
+	$firstName = $response->getFirstName();
+
+	$username = $response->getUsername();
+	var_dump($botId, $firstName, $username);
+
+ ?>
